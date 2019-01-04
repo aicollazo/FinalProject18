51af94fcf828db3fd53b3b19fa7d81a70fb1871c
Ari <acollazo@ucvts.org>
2019-01-04 18:16:57 +0000

    Bread can have personalities?

--- Bread.py
+++ Bread.py
@@ -8,29 +8,31 @@ while A not in ['yes', 'y', 'Y', 'Yes']:
 print("Let's getis this breadis...\n")
 
 class character:
-  def __init__(self, name, year, school, bread, interests):
+  def __init__(self, name, year, school, bread, funfacts, alignment):
     self.name = name
     self.year = year
     self.school = school
     self.bread = bread
-    self.interests = interests
+    self.funfacts = funfacts
+    self.alignment = alignment
   def describe(self):
     print(f"""{self.name}:
     - Year: {self.year}
     - School: {self.school}
     - Bread Type: {self.bread}
-    - Interests: {self.interests}""")
+    - Fun Facts!: {self.funfacts}
+    - Alignment: {self.alignment}""")
     return
-Dirk = character('Dirk', 11, 'APA', 'Whole grain', 'Singing, dancing, watching horror movies with some hot cocoa, hanging out with friends by the lake')
-JC = character('JC', 12, 'UCT','Croissant')
-Mariah = character('Mariah', 12,'AAHS', 'Baguette')
-Mikey = character('Mikey', 12,'MHS', 'Cuban', '')
-Naomi = character('Naomi', 11, 'AIT', 'Naan', '')
+Dirk = character('Dirk', 11, 'APA', 'Whole grain', 'loves singing, dancing, watching horror movies with some hot cocoa, hanging out with friends by the lake', 'Looks like a cinnamon roll and is one, lawful good')
+JC = character('JC', 12, 'UCT','Croissant', 'loves hanging out at starbucks, best friends witht the duolingo owl, enjoys macchiatos and crunching fallen leaves', 'Looks like a cinnamon roll but could kill you, lawful neutral')
+Marie = character('Marie', 12,'AAHS', 'Baguette', 'Super into dermatology, fashion magazines, loves boxing and takes classes at Tiger Bread Schulmann, dog walking', 'Looks like she could kill you, can actually kill you, true neutral ')
+Mikey = character('Mikey', 12,'MHS', 'Cuban', 'loves videogames and cosplay, 80s and 90s hip hop, had his first kiss to sir-mix-alots Baby Got Back', 'Looks like a cinnamon roll but could kill you, chaotic good')
+Naomi = character('Naomi', 11, 'AIT', 'Naan', 'loves coding, looks darn good in suits, grew up on Studio Ghibli, will love you forever if you get her mango icecream', 'Looks like she could kill you, is actually a cinnamon roll, neutral good')
 
 
 Dirk.describe()
 JC.describe()
-Mariah.describe()
+Marie.describe()
 Mikey.describe()
 Naomi.describe()
 
