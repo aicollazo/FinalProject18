b72488e294b954f6c7fd9b5b03c7744e2971f2c9
Ari <acollazo@ucvts.org>
2019-01-14 18:16:09 +0000

    Day 1 done! You met Dirk!

--- Bread.py
+++ Bread.py
@@ -18,13 +18,17 @@ inventory = {
     'Journals': 0,
     'Magazines': 0,
     'Keychains': 0,
-    'Mango Ice cream': 0
+    'Mango Ice cream': 0,
+    'Hearts': 8
     }
 c = input("Check out your inventory by pressing 'i'!")
 if c == "i":
     print_inventory()
-
+if inventory['Hearts'] == 0:
+    print("Oh no, you lost so much love that you couldn't bring yourself to go to prom\n")
+    print("GAME OVER")
 #for reference, this is how to add items to the inventory inventory['Hot cocoa']+=1
+
 
 
 class character:
@@ -46,7 +50,7 @@ class character:
 Dirk = character('Dirk', 11, 'APA', 'Whole grain', 'loves singing, dancing, watching horror movies with some hot cocoa, hanging out with friends by the lake', 'Looks like a cinnamon roll and is one, lawful good')
 JC = character('JC', 12, 'UCT','Croissant', 'loves hanging out at starbucks, best friends with the duolingo owl, enjoys macchiatos and crunching fallen leaves', 'Looks like a cinnamon roll but could kill you, lawful neutral')
 Marie = character('Marie', 12,'AAHS', 'Baguette', 'Super into dermatology, fashion magazines, loves boxing and takes classes at Tiger Bread Schulmann, dog walking', 'Looks like she could kill you, can actually kill you, true neutral ')
-Mikey = character('Mikey', 12,'MHS', 'Cuban', 'loves videogames and cosplay, 80s and 90s hip hop, had his first kiss to sir-mix-alots Baby Got Back', 'Looks like a cinnamon roll but could kill you, chaotic good')
+Xavier = character('Xavier', 12,'MHS', 'Cuban', 'loves videogames and cosplay, 80s and 90s hip hop, had his first kiss to sir-mix-alots Baby Got Back', 'Looks like a cinnamon roll but could kill you, chaotic good')
 Naomi = character('Naomi', 11, 'AIT', 'Naan', 'loves coding, looks darn good in suits, grew up on Studio Ghibli, will love you forever if you get her mango icecream', 'Looks like she could kill you, is actually a cinnamon roll, neutral good')
 
 
@@ -88,7 +92,34 @@ if location == 'bathroom':
 if location == 'classroom':
     print("DIRK: Oh did you get scared by the CPR dummies too?? They're so freaky right!")
 
-print(f"Anyways, I really could use your help! I need to memorise my lines but I have no
+print(f"DIRK: Anyways, I really could use your help! I need to memorise my lines but I have no partner.")
+answer = input("""DIRK: Can you help me?
+A: Sorry I can't! Maybe next time
+B: Sure, what do you need me to do?\n""")
+while answer not in ['A', 'B']:
+  print("DIRK:Huh?? Come on give me an answer!")
+  answer = input()
+if answer == 'A':
+    print("Dirk looks dissapointed but hides it with a smile\n")
+    print("DIRK: Oh...that's alright, I understand you're busy and all..\n")
+    inventory['Hearts']-=1
+    print(f"Oh no! Because you dissapointed Dirk you lost one heart!")
+    print(f"Hearts: {inventory['Hearts']}")
+if answer == 'B':
+    print("""Dirk starts flailing his arms excitedly.
+    DIRK: YAY! Let's get started then!!\n""")
+    print("You and Dirk run lines together in the auditorium, chatting during your session. You've always know him to be the shy type but you never expected him to blush as much as he did when you ran romantic scenes.")
+    print("DIRK: Thank's again for running lines with me, you're so good to me. Oh! Here!\n")
+    inventory['Hot cocoa']+=2
+    print("Dirk gives you two pakcets of hot cocoa! You were such a big help that you gained a heart from the experience!")
+    inventory['Hearts']+=1
+    print_inventory()
+
+print("You go home and think about the day. It was kind of short, wasn't it? Almost as if the programmer couldn't be bothered to narrate your school day because you're supposed to get busy falling in love. Ok I've said too much now.")
+print("DAY 2...")
+print("You wake up at about 6am. You don't feel rested in the slightest. ")
+
+
 
 
 
